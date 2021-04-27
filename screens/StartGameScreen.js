ca70cb3eb0bc29caafa2245c263486104275401b
Margarita Morozova <ritamorozova@yahoo.com>
2021-04-27 16:53:04 -0700

    generate random number

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -3,6 +3,7 @@ import { View, StyleSheet, Text, Button, TouchableWithoutFeedback, Keyboard, Ale
 import Card from '../components/Card';
 import Colors from '../constants/colors';
 import Input from '../components/Input';
+import NumberContainer from '../components/NumberContainer'
 
 const StartGameScreen = props => {
 
@@ -28,12 +29,19 @@ const StartGameScreen = props => {
         setConfirmed(true);
         setSelectedNumber(chosenNumber);
         setEnteredValue('');
+        Keyboard.dismiss();
     };
 
     let confirmedOutput;
 
     if (confirmed) {
-        confirmedOutput = <Text>Chosen Number: {selectedNumber}</Text>
+        confirmedOutput = (
+            <Card style={styles.summaryContainer}>
+                <Text>You selected</Text>
+                <NumberContainer>{selectedNumber}</NumberContainer>
+                <Button title='START GAME' onPress={() => props.onStartGame(selectedNumber)}/>
+            </Card>
+        ) 
     }
 
     return(
@@ -92,6 +100,10 @@ const styles = StyleSheet.create({
     input: {
         width: 50,
         textAlign: 'center',
+    },
+    summaryContainer: {
+        marginTop: 20,
+        alignItems: 'center',
     }
 });
 
