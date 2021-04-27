0193f91e105b9370d5f588b5b3fb09d8f934c97f
Margarita Morozova <ritamorozova@yahoo.com>
2021-04-27 15:45:52 -0700

    style input

---
+++ constants/colors.js
@@ -0,0 +1,4 @@
+export default {
+    primary: '#e24359',
+    accent: '#4370e2',
+};
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,21 +1,42 @@
-import React from 'react';
-import { View, StyleSheet, Text, TextInput, Button } from 'react-native';
-import Card from '../components/Card'
+import React, { useState } from 'react';
+import { View, StyleSheet, Text, Button, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import Card from '../components/Card';
+import Colors from '../constants/colors';
+import Input from '../components/Input';
 
 const StartGameScreen = props => {
 
+    const [enteredValue, setEnteredValue] = useState('');
+
+    const numberInputHandler = inputText => {
+        setEnteredValue(inputText.replace(/[^0-9]/g, ''));
+    };
+
     return(
+      <TouchableWithoutFeedback onPress={() => {
+          Keyboard.dismiss();
+      }}>
         <View style={styles.screen}>
             <Text style={styles.title}>Start a New Game</Text>
             <Card style={styles.inputContainer}>
                 <Text>Select a Number</Text>
-                <TextInput />
+                <Input 
+                    style={styles.input} 
+                    blurOnSubmit 
+                    autoCapitalize='none' 
+                    autoCorrect={false} 
+                    keyboardType='number-pad' 
+                    maxLength={2} 
+                    onChangeText={numberInputHandler}
+                    value={enteredValue}
+                />
                 <View style={styles.buttonContainer}>
-                    <Button title='Reset' onPress={() => {}}/>
-                    <Button title='Confirm' onPress={() => {}}/>
+                    <View style={styles.button}><Button title='Reset' onPress={() => {}} color={Colors.accent} /></View>
+                    <View style={styles.button}><Button title='Confirm' onPress={() => {}} color={Colors.primary} /></View>
                 </View>
                 </Card>
          </View>
+        </TouchableWithoutFeedback>   
     );
 };
 
@@ -39,6 +60,13 @@ const styles = StyleSheet.create({
         width: 300,
         maxWidth: '80%',
         alignItems: 'center',
+    },
+    button : {
+        width: 100,
+    },
+    input: {
+        width: 50,
+        textAlign: 'center',
     }
 });
 
